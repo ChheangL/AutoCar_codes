43f2bbc8f53c543233e4fb624ce9b82f7a2728c4
ChheangL <90783994+ChheangL@users.noreply.github.com>
2022-01-21 13:05:49 +0700

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,14 @@
+from auto_drive_functions import retrieve_angle
+from ImageFrame import Frame
+import cv2
+
+frame1 = Frame(1280,720,10)
+cam = cv2.VideoCapture(0)
+
+
+def main():
+    img = cv2.cvtColor(cam.read()[1],cv2.COLOR_BGR2GRAY)
+    angle = retrieve_angle(s1= 60,hd=5,img = img,frame = frame1,plot=True)
+    print(angle)
+    
+main()
